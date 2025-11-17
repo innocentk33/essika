01bd1705297aa0731a011fa63fb28c1d9f2e78eb
innocentk33 <innocentk33@gmail.com>
2025-11-17 23:30:17 +0100

    Add default category names and images for category management

---
+++ lib/core/const/default_data.dart
@@ -0,0 +1,25 @@
+const List<String> categoryImages = [
+  'assets/images/category_icons/shopping.png',
+  'assets/images/category_icons/food.png',
+  'assets/images/category_icons/transportation.png',
+  'assets/images/category_icons/entertainment.png',
+  'assets/images/category_icons/health.png',
+  'assets/images/category_icons/utilities.png',
+  'assets/images/category_icons/education.png',
+  'assets/images/category_icons/travel.png',
+  'assets/images/category_icons/gifts.png',
+  'assets/images/category_icons/others.png',
+];
+
+const List<String> defaultCategoryNames = [
+  'Vêtements',
+  'Nourriture',
+  'Transport',
+  'Divertissement',
+  'Santé',
+  'Services publics',
+  'Éducation',
+  'Voyage',
+  'Cadeaux',
+  'Autres',
+];
